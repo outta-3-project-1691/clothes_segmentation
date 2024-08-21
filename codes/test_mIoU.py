7aaa8498ddb1b0405b1e5137ca9d28c083be54bb
esillileu yun <esillileu@gmail.com>
2024-08-21 23:45:15 +0900

    #3 fix miou to multiclass & dataloader

--- codes/test_mIoU.py
+++ codes/test_mIoU.py
@@ -1,16 +1,13 @@
 import torch
+from torch.functional import F
 
 # preds, labels -> torch.Tensor
-def calculate_miou(preds, labels, num_classes=7):
-
-    preds = preds.to('cuda')
-    labels = labels.to('cuda')
-
+def calculate_miou(preds, targets, num_classes=7):
     ious = []
     for i in range(num_classes):
-        tp = torch.sum((preds == i) & (labels == i)).float()
-        fp = torch.sum((preds == i) & (labels != i)).float()
-        fn = torch.sum((preds != i) & (labels == i)).float()
+        tp = torch.sum((preds == i) & (targets == i)).float()
+        fp = torch.sum((preds == i) & (targets != i)).float()
+        fn = torch.sum((preds != i) & (targets == i)).float()
 
         # IoU = Intersection (TP) / Union (TP + FP + FN)
         iou = tp / (tp + fp + fn) if (tp + fp + fn) > 0 else torch.tensor(0.0).to('cuda')
@@ -18,27 +15,26 @@ def calculate_miou(preds, labels, num_classes=7):
     
     return torch.mean(torch.tensor(ious)).item()
 
-def evaluate_model(img_list, gt_list, model, num_classes=7):
-
+def evaluate_mIoU(model, dataloader, num_classes=7, device='cpu'):
     model.eval()
-
-    all_preds = []
-    all_labels = []
+    total_mIoU_score = 0
+    total_batches = 0
 
     with torch.no_grad():
-        for inputs, labels in zip(img_list, gt_list):
-
-            inputs = inputs.to('cuda')
-            labels = labels.to('cuda')
+        for inputs, targets in dataloader:
             
-            # output -> (batch_size, num_classes, height, width)
-            outputs = model(inputs)
-            _, preds = torch.max(outputs, 1)
+            # 모델 예측
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)  # outputs: [b, 7, 128, 128]
+            
+            # 소프트맥스 후 argmax로 예측된 클래스 선택
+            preds = torch.argmax(F.softmax(outputs, dim=1), dim=1)  # preds: [b, 128, 128]
 
-            all_preds.append(preds)
-            all_labels.append(labels)
+            # F1 스코어 계산
+            mIoU_score = calculate_miou(preds, targets, num_classes)
+            total_mIoU_score += mIoU_score
+            total_batches += 1
     
-    all_preds = torch.cat(all_preds, dim=0)
-    all_labels = torch.cat(all_labels, dim=0)
-    
-    return calculate_miou(all_preds, all_labels, num_classes)
+    mean_mIoU_score = total_mIoU_score / total_batches
+
+    return mean_mIoU_score
