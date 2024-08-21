cfedaee9e6cba14e4fab160d38e9443daae74e1d
esillileu <esillileu@gmail.com>
2024-08-21 11:56:31 +0000

    #2 add showing label

--- codes/show_result.py
+++ codes/show_result.py
@@ -4,14 +4,13 @@ import matplotlib.pyplot as plt
 
 def show_segment(image_tensor):
     colors = {
-        0: 
-        1: [255, 0, 0],   # Red
-        2: [0, 255, 0],   # Green
-        3: [0, 0, 255],   # Blue
-        4: [255, 255, 0], # Yellow
-        5: [255, 0, 255], # Magenta
-        6: [0, 255, 255], # Cyan
-        6: # Gray
+        0: [128, 128, 128], # Gray
+        1: [255, 0, 0],     # Red
+        2: [0, 255, 0],     # Green
+        3: [0, 0, 255],     # Blue
+        4: [255, 255, 0],   # Yellow
+        5: [255, 0, 255],   # Magenta
+        6: [0, 255, 255],   # Cyan
     }
 
     # 3채널 컬러 이미지로 변환
